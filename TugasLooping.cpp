3983297eabdccfc55ba68333bd47f04367da6699
envikaaa <envikapriyatna31@gmail.com>
2026-03-31 08:59:37 +0700

    int i = 2;

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -7,8 +7,15 @@ using namespace std;
 int angka = 0;
 int pilihan = 0;
 
+// =============================================
+// FUNCTION PENGECEKAN
+// =============================================
+
 // Function untuk mengecek bilangan prima menggunakan while loop
 bool isPrima(int n) {
     if (n <= 1) return false;
     if (n == 2  n == 3) return true;
+
+    int i = 2;
+    while (i * i <= n)
 }
